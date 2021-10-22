6083f9ec8ffd964316364022d5cba9e6c53f3b12
Don616 <contatodonilo@gmail.com>
2021-10-22 14:15:58 -0300

    começo do banco de dados da batalha naval

---
+++ banco-de-dados/jogo-xadrez.sql
@@ -0,0 +1,46 @@
+/*
+
+Escolher um jogo;
+Criar entre 3 ~ 5 tabelas;
+Elas precisam se relacionar em si;
+Será contabilizado como tabela também tabelas associativas;
+Busquem usar relacionamento forte, fraco, um pra n, zero pra 1 etc etc. Máximo possível;
+Declarem as regras do jogo;
+
+Jogo escolhido: Batalha Naval;
+
+Tabelas:
+ - Jogador1 = {
+	IdJogador1;
+    Nome;
+    
+ };
+ - Jogador2 = {
+		IdJogador1;
+    Nome;
+    
+ };
+ - Tabuleiro = {
+	
+ };
+ - Peças = {
+ 
+	Porta-Aviões = 5;
+    Navios-Tanque = 4;
+    Contra-Torpedos = 3;
+    Submarinos = 2;
+    Minas-Marítimas = 1;
+    
+ };
+ 
+ 
+
+
+*/
+
+CREATE DATABASE BATALHA_NAVAL;
+
+USE BATALHA_NAVAL;
+
+
+
