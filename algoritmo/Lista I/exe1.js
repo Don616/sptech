7702aed70ab3576fbbaaa52d20779d02046c190e
Don616 <contatodonilo@gmail.com>
2021-11-08 16:49:42 -0200

    colocando em js

--- algoritmo/Lista I/exe1.js
+++ algoritmo/Lista I/exe1.js
@@ -1,15 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-</body>
-</html>
-<script>
+/*
+COLE EM UM HTML!!!!
+*/ 
 
     document.title = "Exe 1"
     var frutas = ['banana','maçã','goiaba','tomate','morango','melão','melancia','fruta do conde']
@@ -34,4 +25,4 @@
 
     })
     
-</script>
+
--- algoritmo/Lista I/exe2.js
+++ algoritmo/Lista I/exe2.js
@@ -1,16 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
-<script>
+/*
+COLE EM UM HTML!!!!
+*/ 
 
     document.title = `Exe 2`;
     var viagens = [];
@@ -73,4 +63,3 @@
 
 
 
-</script>
