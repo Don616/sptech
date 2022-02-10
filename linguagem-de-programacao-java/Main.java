75f5818823eaed300ecd31b2ce6bf3d0d9cb0208
Don616 <contatodonilo@gmail.com>
2022-02-10 19:43:32 -0300

    subindo exes de java

---
+++ linguagem-de-programacao-java/Main.java
@@ -0,0 +1,18 @@
+/*
+
+Exercício para programar em java.
+Por: Don616
+
+
+*/
+public class Main {
+    
+    public static void main(String[] args){
+
+        System.out.print("c\n"); // O '\n' pula a linha forçadamente
+        System.out.println("\ta"); // Para dar um 'tab' basta digitar '\t'
+        System.out.println("\t\tb");
+
+    }
+
+}
