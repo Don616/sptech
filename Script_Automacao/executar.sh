90695134458542f89a47067696a03683b1c18b59
Don616 <contatodonilo@gmail.com>
2022-06-03 19:33:08 -0300

    att script executar.sh

--- Script_Automacao/executar.sh
+++ Script_Automacao/executar.sh
@@ -1,6 +1,6 @@
-sudo su
+
 xauth add $(xauth -f /home/urubu100/.Xauthority list|tail -l)
 xauth add $(xauth -f /home/urubu100/.Xauthority list|tail -l)
 export DISPLAY=:0.0
 sudo docker start mysql-totem
-java -jar TotemSystemGUI.jar
+exit
